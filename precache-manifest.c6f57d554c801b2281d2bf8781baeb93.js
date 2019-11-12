ca93ce9b5b5c0610ec57f06aa1c21c4a0fcebfec
Daniel <caudaDelphini@gmail.com>
2019-11-12 15:09:56 -0500

    Updates

---
+++ precache-manifest.c6f57d554c801b2281d2bf8781baeb93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0dd661749a9cf608b0fd42d2a927ee63",
+    "url": "/20191111b-chronolog/index.html"
+  },
+  {
+    "revision": "3ece03d4b610c96e2370",
+    "url": "/20191111b-chronolog/static/css/main.d0f1652b.chunk.css"
+  },
+  {
+    "revision": "e887cd51d2ffa44d923a",
+    "url": "/20191111b-chronolog/static/js/2.2afe89c9.chunk.js"
+  },
+  {
+    "revision": "3ece03d4b610c96e2370",
+    "url": "/20191111b-chronolog/static/js/main.d3c8e51b.chunk.js"
+  },
+  {
+    "revision": "31d2472880a66d298eeb",
+    "url": "/20191111b-chronolog/static/js/runtime-main.75232430.js"
+  }
+]);
